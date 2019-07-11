be7e6c2539af49ffb50881dd3d117601c2aa608f
azimut3 <i.horchynskyi@gmail.com>
2019-07-12 00:50:51 +0300

    App working according to task

--- app/src/main/java/com/example/androidapp/HumanActivity.java
+++ app/src/main/java/com/example/androidapp/HumanActivity.java
@@ -2,7 +2,12 @@ package com.example.androidapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.example.androidapp.data.Human;
 
 public class HumanActivity extends AppCompatActivity {
 
@@ -10,5 +15,14 @@ public class HumanActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_human);
+
+        Intent intent = getIntent();
+        Human human = intent.getParcelableExtra("human");
+        EditText humanInfo = findViewById(R.id.resultField);
+        humanInfo.setText(human.toString());
+    }
+
+    public void closeAction(View view){
+        finish();
     }
 }
--- app/src/main/java/com/example/androidapp/MainActivity.java
+++ app/src/main/java/com/example/androidapp/MainActivity.java
@@ -10,30 +10,36 @@ import android.widget.EditText;
 import com.example.androidapp.data.Human;
 
 public class MainActivity extends AppCompatActivity {
-    EditText name,age,surname;
+    EditText nameField = null;
+    EditText ageField = null;
+    EditText surnameField = null;
     Button submitBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        name = findViewById(R.id.nameField);
-        surname = findViewById(R.id.surnameField);
-        age = findViewById(R.id.ageField);
+        nameField = findViewById(R.id.nameField);
+        surnameField = findViewById(R.id.surnameField);
+        ageField = findViewById(R.id.ageField);
         submitBtn = findViewById(R.id.submitBtn);
 
         submitBtn.setOnClickListener((e) -> submitAction());
     }
 
     public void submitAction(){
-        Human human = saveHuman();
-        Intent humanSubmitIntent = new Intent();
-        humanSubmitIntent.putExtra("Human", human);
-        startActivity(humanSubmitIntent);
+        String name = nameField.getText().toString();
+        String surname = surnameField.getText().toString();
+        String age = ageField.getText().toString();
+        if (name != null && surname != null && age != null) {
+            Human human = saveHuman(name, surname, age);
+            Intent humanSubmitIntent = new Intent(this, HumanActivity.class);
+            humanSubmitIntent.putExtra("human", human);
+            startActivity(humanSubmitIntent);
+        }
     }
 
-    private Human saveHuman(){
-        return new Human(name.getText().toString(), surname.getText().toString(),
-                age.getText().toString());
+    private Human saveHuman(String name, String surname, String age){
+        return new Human(name, surname, age);
     }
 }
